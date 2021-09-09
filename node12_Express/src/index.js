26a3aeaef014e2edaed66f1979020eca5acba683
jianzhicvs <62820666+jianzhicvs@users.noreply.github.com>
2021-09-09 20:33:50 +0800

    Update index.js

--- node12_Express/src/index.js
+++ node12_Express/src/index.js
@@ -1,1 +1,28 @@
-
+var express = require('express');
+var app = express();
+ 
+app.use('/public', express.static('public'));
+ 
+app.get('/index.html', function (req, res) {
+   res.sendFile( __dirname + "/" + "index.html" );
+})
+ 
+app.get('/value_get', function (req, res) {
+ 
+   // 输出 JSON 格式
+   var response = {
+       "first_name":req.query.first_name,
+       "last_name":req.query.last_name
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+})
+ 
+var server = app.listen(2010, function () {//云函数监听端口
+ 
+  var host = server.address().address
+  var port = server.address().port
+ 
+  console.log("应用实例，访问地址为 http://%s:%s", host, port)
+ 
+})
